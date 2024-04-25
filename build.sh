c463237f68f6ae7dfc505b7f4847ec6e7851da1c
xuyao <xuyao@thoughtworks.com>
2024-04-25 20:41:46 +0800

    first commit

--- build.sh
+++ build.sh
@@ -10,7 +10,6 @@ yarn config set registry http://registry.npm.taobao.org
 yarn config set sass-binary-site http://npm.taobao.org/mirrors/node-sass
 yarn config set chromedriver_cdnurl http://cdn.npm.taobao.org/dist/chromedriver
 
-yarn config set cache-folder /yarn
 yarn
 
 export NODE_ENV=dev
